3895954769617b85dbb2e2e983eb0650ab28ad80
guzillo <guzillo2077@gmail.com>
2025-05-04 10:26:52 +0200

    Add closing and opening modal logic with event listeners

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const myLibrary = [];
 const booksContainer = document.getElementById('books-container');
+const addBookModal = document.getElementById('add-book-modal');
+const addBookBtn = document.getElementById('add-book-button');
+const closeBtn = document.getElementsByClassName('close')[0];
 function Book(title, author, pagesCount, releaseYear, readStatus) {
     this.title = title;
     this.author = author;
@@ -20,12 +23,21 @@ function displayBooks() {
         booksContainer.innerHTML +=
         `
         <div class="book-card">
-            <h2 class="book-title">${title}</h2>
-            <p id="book-author">author: ${author}</p>
-            <p id="book-pages">Pages: ${pagesCount}</p>
-            <p id="book-release-year">Release year: ${releaseYear}</p>
-            <p id="book-reading-status">Reading status: ${readStatus}</p>
+            <h2 class="book-title">${title ? title : 'N/A'}</h2>
+            <p id="book-author">author: ${author ? author : 'N/A'}</p>
+            <p id="book-pages">Pages: ${pagesCount ? pagesCount : 'N/A'}</p>
+            <p id="book-release-year">Release year: ${releaseYear ? releaseYear : 'N/A'}</p>
+            <p id="book-reading-status">Reading status: ${readStatus ? readStatus : 'N/A'}</p>
         </div>
         `
     }
 }
+
+addBookBtn.addEventListener('click', () => {
+    addBookModal.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    addBookModal.style.display = 'none';
+});
+
